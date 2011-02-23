f0d57d3a462b8f535246aa8a8c1b82c62bd0b7c6
Gabriel Couto <gmcouto@gmail.com>
2011-02-23 08:23:17 -0300

    Corrected for latest CB build

--- src/main/java/com/nijikokun/bukkit/Permissions/Permissions.java
+++ src/main/java/com/nijikokun/bukkit/Permissions/Permissions.java
@@ -35,39 +35,14 @@ public class Permissions extends JavaPlugin {
     public static String name = "Permissions";
     public static String codename = "Hacked Permissions by AnjoCaido";
     public static String version = "2.0";
-    public static PermissionHandler Security = new Control(null);
+    public static PermissionHandler Security = null;
     public static Misc Misc = new Misc();
     public static Server Server;
     private Listener Listener = null;
     private DefaultConfiguration config = null;
     private GroupManager groupManager;
 
-    public Permissions(PluginLoader pluginLoader, Server instance, PluginDescriptionFile desc, File folder, File plugin, ClassLoader cLoader) {
-        super(pluginLoader, instance, desc, folder, plugin, cLoader);
-        Server = this.getServer();
-        Plugin p = this.getServer().getPluginManager().getPlugin("GroupManager");
-        if (p != null) {
-            if (!this.getServer().getPluginManager().isPluginEnabled(p)) {
-                try {
-                    this.getServer().getPluginManager().enablePlugin(p);
-                    this.getServer().getPluginManager().enablePlugin(this);
-                } catch (Exception e) {
-                }
-            }
-        } else {
-            File[] listFiles = this.getDataFolder().getParentFile().listFiles();
-            for (File f : listFiles) {
-                if (f.getName().equalsIgnoreCase("GroupManager.jar")) {
-                    try {
-                        this.getServer().getPluginManager().loadPlugin(f);
-                        this.getServer().getPluginManager().enablePlugin(this);
-                    } catch (Exception e) {
-                    }
-                }
-            }
-        }
-    }
-
+    @Override
     public void onDisable() {
         //compiled code
         //throw new RuntimeException("Compiled Code");
@@ -76,24 +51,28 @@ public class Permissions extends JavaPlugin {
         System.out.println("Fake " + pdfFile.getName() + " version " + pdfFile.getVersion() + " is disabled!");
     }
 
+    @Override
     public void onEnable() {
-        //compiled code
         Server = this.getServer();
-        Plugin p = this.getServer().getPluginManager().getPlugin("GroupManager");
         PluginDescriptionFile pdfFile = this.getDescription();
-        if (p != null) {
-            if (!this.getServer().getPluginManager().isPluginEnabled(p)) {
-                this.getServer().getPluginManager().enablePlugin(p);
+
+        if (Security == null) {//make sure we have only one instance
+            Plugin p = this.getServer().getPluginManager().getPlugin("GroupManager");
+            if (p != null) {
+                if (!this.getServer().getPluginManager().isPluginEnabled(p)) {
+                    this.getServer().getPluginManager().enablePlugin(p);
+                }
+                GroupManager gm = (GroupManager) p;
+                groupManager = gm;
+                Security = gm.getPermissionHandler();
+            } else {
+                System.err.println("OOOPS! Fake " + pdfFile.getName() + " version " + pdfFile.getVersion() + " couldn't find GroupManager!");
+                this.getPluginLoader().disablePlugin(this);
             }
-            GroupManager gm = (GroupManager) p;
-            groupManager = gm;
-            Security = gm.getPermissionHandler();
-            // EXAMPLE: Custom code, here we just output some info so we can check all is well         
-            System.out.println("Fake " + pdfFile.getName() + " version " + pdfFile.getVersion() + " is enabled!");
-        } else {
-            System.err.println("OOOPS! Fake " + pdfFile.getName() + " version " + pdfFile.getVersion() + " couldn't find GroupManager!");
-            this.getPluginLoader().disablePlugin(this);
         }
+        // EXAMPLE: Custom code, here we just output some info so we can check all is well
+        System.out.println("Fake " + pdfFile.getName() + " version " + pdfFile.getVersion() + " is enabled!");
+
     }
 
     private void registerEvents() {
@@ -114,7 +93,7 @@ public class Permissions extends JavaPlugin {
     }
 
     private void checkEnable() {
-        if (!this.isEnabled()) {
+        if (!this.isEnabled() && Security == null) {
             this.getServer().getPluginManager().enablePlugin(this);
         }
     }
